d25a1d7b8ef665f7255ed3c0f69fdc55c9eaf8c4
debsayan <87645712+debsayan@users.noreply.github.com>
2022-01-15 14:06:52 +0530

    Add files via upload

---
+++ Project 9.js
@@ -0,0 +1,20 @@
+var box;
+function setup() {
+  createCanvas(1600,1600);
+box=createSprite(400, 400, 60, 60)
+console.log(box)
+}
+
+function draw() 
+{
+  background("red");
+  if(keyIsDown(RIGHT_ARROW)){
+   background("cyan")
+  }
+drawSprites()
+
+}
+
+
+
+
